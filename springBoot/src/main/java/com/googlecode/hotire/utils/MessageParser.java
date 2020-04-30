94002549b01d7e02129a4acf9c2e2b4327829189
hotire <gngh0101@gmail.com>
2020-04-30 23:57:22 +0900

    Simplify parseMessage in MessageParser

--- springBoot/src/main/java/com/googlecode/hotire/utils/MessageParser.java
+++ springBoot/src/main/java/com/googlecode/hotire/utils/MessageParser.java
@@ -4,12 +4,10 @@ import com.googlecode.hotire.annoation.FixedString;
 import org.springframework.core.annotation.AnnotationUtils;
 
 import java.beans.PropertyDescriptor;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class MessageParser {
@@ -39,74 +37,63 @@ public class MessageParser {
 	}
 
 	public static String parseMessage(String message, Object parseObject) {
-		List<Field> fields = TypeUtils.getInstance().getInheritedFields(parseObject.getClass());
-		int fieldIndex = 0;
-		byte[] data = null;
-		try {
-			data = message.getBytes("euc-kr");
-		} catch (UnsupportedEncodingException e1) {
-			// ignore
-		}
+		final AtomicInteger fieldIndex = new AtomicInteger();
 
-		for (Field field : fields) {
-			try {
-				FixedString fixedStringInfo = field.getAnnotation(FixedString.class);
-				if (Objects.isNull(fixedStringInfo)) {
-					continue;
-				}
-				int strLength = fixedStringInfo.value();
-				byte[] fieldData = Arrays.copyOfRange(data, fieldIndex, fieldIndex + strLength);
+		Arrays.stream(parseObject.getClass().getDeclaredFields())
+			  .filter(field -> Objects.nonNull(AnnotationUtils.findAnnotation(field, FixedString.class)))
+			  .forEach(field -> {
+				  try {
+					  final FixedString fixedStringInfo = field.getAnnotation(FixedString.class);
+					  final byte[] data = message.getBytes("euc-kr");
+					  final int strLength = fixedStringInfo.value();
+					  final byte[] fieldData = Arrays.copyOfRange(data, fieldIndex.get(), fieldIndex.get() + strLength);
+					  final String value = new String(fieldData, "euc-kr");
 
-				String value = new String(fieldData, "euc-kr");
+					  new PropertyDescriptor(field.getName(), parseObject.getClass()).getWriteMethod().invoke(parseObject, value);
+					  fieldIndex.set(fieldIndex.get() + strLength);
+				  } catch (Exception e) {
+					  // ignore
+				  }
+			  });
 
-				new PropertyDescriptor(field.getName(), parseObject.getClass()).getWriteMethod().invoke(parseObject,
-						value);
-				fieldIndex += strLength;
-			} catch (Exception e) {
-				// ignore
-			}
-		}
-		if (message.length() <= fieldIndex) {
+		if (message.length() <= fieldIndex.get()) {
 			return "";
 		}
-		return message.substring(fieldIndex);
+
+		return message.substring(fieldIndex.get());
 	}
 
 	public static String cutStringByByteLength(String s, int byteLimit) {
 
-		byte[] data = s.getBytes();
-		if (data.length <= byteLimit) {
-			return s;
-		}
 		int n = Math.min(byteLimit - 1, s.length() - 1);
-		do {
+
+		while (s.getBytes().length > byteLimit) {
 			s = s.substring(0, n--);
-		} while (data.length > byteLimit);
+		}
 
 		return s;
 	}
 
 	public static String fillAfterSpace(String s, int byteLimit) {
-		StringBuilder builder = new StringBuilder();
+		final StringBuilder builder = new StringBuilder();
 		builder.append(s);
-		int stringLen = 0;
 
-		byte[] data = s.getBytes();
-		stringLen = data.length;
+		final int stringLen = s.getBytes().length;
 		for (int i = 0; i < byteLimit - stringLen; i++) {
 			builder.append(" ");
 		}
+
 		return builder.toString();
 	}
 
 	public static String fillBeforeZeroSpace(String s, int byteLimit) {
-		StringBuilder builder = new StringBuilder();
+		final StringBuilder builder = new StringBuilder();
+		final int stringLen = s.getBytes().length;
 
-		byte[] data = s.getBytes();
-		int stringLen = data.length;
 		for (int i = 0; i < byteLimit - stringLen; i++) {
 			builder.append("0");
 		}
+
 		builder.append(s);
 		return builder.toString();
 	}
