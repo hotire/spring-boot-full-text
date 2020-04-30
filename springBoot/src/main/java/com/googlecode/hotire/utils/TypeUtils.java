826b9969dd41b6ee1948e7a0f041435399ac417a
hotire <gngh0101@gmail.com>
2020-04-30 23:23:40 +0900

    Add final to variable

--- springBoot/src/main/java/com/googlecode/hotire/utils/TypeUtils.java
+++ springBoot/src/main/java/com/googlecode/hotire/utils/TypeUtils.java
@@ -1,16 +1,13 @@
 package com.googlecode.hotire.utils;
 
 import com.googlecode.hotire.annoation.Order;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-/**
- * 
- * @author : hoTire
- * @comment : Lookup own and inherited fields   
- * Singleton holder pattern
- */
+
+
 public class TypeUtils {
 	private TypeUtils() {}
 	private final static class SingletonHolder {
@@ -19,8 +16,8 @@ public class TypeUtils {
 	public static TypeUtils getInstance() {
 		return SingletonHolder.instance;
 	}
-	public List<Field> getInheritedFields(Class<?> type) {
-        List<Field> fields = new ArrayList<>();
+	public List<Field> getInheritedFields(final Class<?> type) {
+        final List<Field> fields = new ArrayList<>();
         for (Class<?> c = type; c != null; c = c.getSuperclass()) {
             fields.addAll(Arrays.asList(c.getDeclaredFields()));
         }
@@ -28,7 +25,7 @@ public class TypeUtils {
         return fields;
     }
 	
-	public void sortFieldsByOrder(List<Field> fields) {
+	public void sortFieldsByOrder(final List<Field> fields) {
 		fields.sort((o1, o2) -> {
 			Order order1 = o1.getAnnotation(Order.class);
 			Order order2 = o2.getAnnotation(Order.class);
