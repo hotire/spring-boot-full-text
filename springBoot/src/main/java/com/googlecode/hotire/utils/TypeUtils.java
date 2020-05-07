a253a0a2534b99c48d7eeae82315d0e4e46de71c
hotire <gngh0101@gmail.com>
2020-05-08 00:07:50 +0900

    Add Deprecated to TypeUtils

--- springBoot/src/main/java/com/googlecode/hotire/utils/TypeUtils.java
+++ springBoot/src/main/java/com/googlecode/hotire/utils/TypeUtils.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Deprecated
 public class TypeUtils {
 	private TypeUtils() {}
 	private final static class SingletonHolder {
