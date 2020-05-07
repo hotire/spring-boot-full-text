632bc9f57cc69b338cf513910dcf4a3ee446933d
hotire <gngh0101@gmail.com>
2020-05-08 00:02:50 +0900

    Implment BaseMessage

--- springBoot/src/main/java/com/googlecode/hotire/model/RequestMessage.java
+++ springBoot/src/main/java/com/googlecode/hotire/model/RequestMessage.java
@@ -1,16 +1,13 @@
 package com.googlecode.hotire.model;
 
 import com.googlecode.hotire.annoation.FixedString;
-import com.googlecode.hotire.annoation.Order;
 import com.googlecode.hotire.constants.MessageFieldType;
 import lombok.Data;
 
 @Data
-public class RequestMessage extends BaseMessage {
-	@FixedString(value = 8, type = MessageFieldType.ALPHABET)
-	@Order(1)
+public class RequestMessage implements BaseMessage {
+	@FixedString(value = 8, type = MessageFieldType.ALPHABET, order = 1)
 	private String organizationCode;
-	@FixedString(value = 20, type = MessageFieldType.NUMERIC)
-	@Order(2)
+	@FixedString(value = 20, type = MessageFieldType.NUMERIC, order = 2)
 	private String chaserNumber;
 }
--- springBoot/src/main/java/com/googlecode/hotire/model/ResponseMessage.java
+++ springBoot/src/main/java/com/googlecode/hotire/model/ResponseMessage.java
@@ -1,14 +1,13 @@
 package com.googlecode.hotire.model;
 
 import com.googlecode.hotire.annoation.FixedString;
-import com.googlecode.hotire.annoation.Order;
 import com.googlecode.hotire.constants.MessageFieldType;
 import lombok.Data;
 
 @Data
-public class ResponseMessage extends BaseMessage {
-	@FixedString(value=4, type=MessageFieldType.ALPHABET) @Order(1)
+public class ResponseMessage implements BaseMessage {
+	@FixedString(value = 4, type = MessageFieldType.ALPHABET, order = 1)
 	private String responseCode;
-	@FixedString(value=20, type=MessageFieldType.NUMERIC) @Order(2)
+	@FixedString(value = 20, type = MessageFieldType.NUMERIC, order = 2)
 	private String chaserNumber;
 }
