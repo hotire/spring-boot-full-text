c8d3b9973d9eac3d0ec6f6987fe610ea469ee7de
hotire <gngh0101@gmail.com>
2020-05-08 00:04:31 +0900

    Add Configuration annoation

--- springBoot/src/main/java/com/googlecode/hotire/security/SecurityConfig.java
+++ springBoot/src/main/java/com/googlecode/hotire/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.googlecode.hotire.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
@@ -12,6 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(final WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/css/**", "/script/**", "image/**", "/fonts/**", "lib/**");
 	}
+
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
 		http.authorizeRequests()
