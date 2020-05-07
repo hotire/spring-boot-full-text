00c3fc0c7aa4d5b0fe3a275e464a6afee37a9b9b
hotire <gngh0101@gmail.com>
2020-05-08 00:02:25 +0900

    Fix class to interface in BaseMessage

--- springBoot/src/main/java/com/googlecode/hotire/model/BaseMessage.java
+++ springBoot/src/main/java/com/googlecode/hotire/model/BaseMessage.java
@@ -3,8 +3,8 @@ package com.googlecode.hotire.model;
 import com.googlecode.hotire.utils.MessageParser;
 
 
-public class BaseMessage {
-	public String getMessage() {
+public interface BaseMessage {
+	default String getMessage() {
 		return MessageParser.getMessage(this);
 	}
 }
