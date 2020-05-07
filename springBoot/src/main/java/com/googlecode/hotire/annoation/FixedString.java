eb5786d9bfc78d8e9a2f644599b34a8ce370e4be
hotire <gngh0101@gmail.com>
2020-05-08 00:03:11 +0900

    Add FIELD_COMPARATOR

--- springBoot/src/main/java/com/googlecode/hotire/annoation/FixedString.java
+++ springBoot/src/main/java/com/googlecode/hotire/annoation/FixedString.java
@@ -6,6 +6,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.util.Comparator;
 
 
 @Target(ElementType.FIELD)
@@ -13,4 +15,11 @@ import java.lang.annotation.Target;
 public @interface FixedString {
 	int value();
 	MessageFieldType type();
+	int order();
+
+	Comparator<Field> FIELD_COMPARATOR = (o1, o2) -> {
+		final int order1 = o1.getAnnotation(FixedString.class).order();
+		final int order2 = o2.getAnnotation(FixedString.class).order();
+		return order1 - order2;
+	};
 }
