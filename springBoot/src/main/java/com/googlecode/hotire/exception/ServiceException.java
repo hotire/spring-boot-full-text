6fa65cb0cb11ae2f76494f6bafe9b3d3a16c4073
hotire <gngh0101@gmail.com>
2020-05-13 01:46:54 +0900

    Add ResponseStatus

--- springBoot/src/main/java/com/googlecode/hotire/exception/ServiceException.java
+++ springBoot/src/main/java/com/googlecode/hotire/exception/ServiceException.java
@@ -1,5 +1,9 @@
 package com.googlecode.hotire.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "ServiceException")
 @SuppressWarnings("serial")
 public class ServiceException extends BaseException {
 	public ServiceException(final String message) {
