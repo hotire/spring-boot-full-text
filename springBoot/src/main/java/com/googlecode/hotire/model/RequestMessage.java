bc3620aff2fee95a9f619e2f6fcb2a56616da17c
hotire <gngh0101@gmail.com>
2020-05-13 01:39:15 +0900

    Use debugId instead of chaserNumber

--- springBoot/src/main/java/com/googlecode/hotire/model/RequestMessage.java
+++ springBoot/src/main/java/com/googlecode/hotire/model/RequestMessage.java
@@ -9,5 +9,6 @@ public class RequestMessage implements BaseMessage {
 	@FixedString(value = 8, type = MessageFieldType.ALPHABET, order = 1)
 	private String organizationCode;
 	@FixedString(value = 20, type = MessageFieldType.NUMERIC, order = 2)
-	private String chaserNumber;
+	private String debugId;
 }
+chaserNumber
