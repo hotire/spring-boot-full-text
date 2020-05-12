b49465bc6b717d9c4c0407c4e1772bbe11e3fede
hotire <gngh0101@gmail.com>
2020-05-13 01:47:54 +0900

    Use lombok getter

--- springBoot/src/main/java/com/googlecode/hotire/exception/BaseException.java
+++ springBoot/src/main/java/com/googlecode/hotire/exception/BaseException.java
@@ -1,13 +1,13 @@
 package com.googlecode.hotire.exception;
 
 
+import lombok.Getter;
+
 @SuppressWarnings("serial")
 public class BaseException extends RuntimeException {
+	@Getter
 	private final String message;
 	public BaseException(final String message) {
 		this.message = message;
-	}
-	public String getMessage() {
-		return message;
 	}
 }
