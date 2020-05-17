8e2d794f340f65c8f1633ba6d30d5a7216bb7470
hotire <gngh0101@gmail.com>
2020-05-18 01:47:02 +0900

    Add

--- springBoot/src/main/java/com/googlecode/hotire/exception/BaseException.java
+++ springBoot/src/main/java/com/googlecode/hotire/exception/BaseException.java
@@ -2,7 +2,10 @@ package com.googlecode.hotire.exception;
 
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "INTERNAL SERVER ERROR")
 @SuppressWarnings("serial")
 public class BaseException extends RuntimeException {
 	@Getter
