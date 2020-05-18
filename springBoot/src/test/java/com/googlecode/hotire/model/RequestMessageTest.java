9fb1fd17b06e8ec247b153b50b44d2240617aa10
hotire <gngh0101@gmail.com>
2020-05-19 01:55:03 +0900

    Remove public keyword

--- springBoot/src/test/java/com/googlecode/hotire/model/RequestMessageTest.java
+++ springBoot/src/test/java/com/googlecode/hotire/model/RequestMessageTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RequestMessageTest {
+class RequestMessageTest {
 
     @Test
     void getOrganizationCode() {
